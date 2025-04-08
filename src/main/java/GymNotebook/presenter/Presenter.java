14dd907e56d88ce5a13f742afe2cdb4bfbff4686
Chivta <arvlas78@gmail.com>
2025-04-08 19:40:28 +0300

    Added Presenter.

---
+++ src/main/java/GymNotebook/presenter/Presenter.java
@@ -0,0 +1,14 @@
+package GymNotebook.presenter;
+
+import GymNotebook.view.UIManager;
+
+public class Presenter {
+    private UIManager ui;
+    public Presenter(UIManager uiManager) {
+        ui = uiManager;
+    }
+
+    public void OpenExerciseView(){
+
+    }
+}
