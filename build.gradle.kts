fd65d4f8b2bc0b227e33ab5ecfd6e7a2aa1397c8
Chivta <arvlas78@gmail.com>
2025-04-11 20:43:59 +0300

    Added necessary files for building app.

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("application")
+}
+
+application {
+    mainClass = "GymNotebook.Main"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events ("passed", "skipped", "failed") // Покажемо тільки важливі події
+        showStandardStreams = true // Виводить стандартні потоки (System.out, System.err) в консоль
+    }
+}
