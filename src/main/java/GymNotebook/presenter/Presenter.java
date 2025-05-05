b702fd54bf2397e34ba2692417b9e39509b8b7ce
Chivta <arvlas78@gmail.com>
2025-05-05 20:51:31 +0300

    Removed unused import for List in Presenter.java.

--- src/main/java/GymNotebook/presenter/Presenter.java
+++ src/main/java/GymNotebook/presenter/Presenter.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.IOException;
-import java.util.List;
 
 public class Presenter {
     private final UIManager ui;
