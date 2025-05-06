572ce4da0b9594f8488b5622d3ceecd366e415de
Chivta <arvlas78@gmail.com>
2025-05-06 16:01:03 +0300

    Refactored `ExerciseCreationWindow` to use the `WindowState` interface for state management. Replaced the `State` enum with individual state classes. Improved input validation and error handling for better modularity and maintainability.

--- src/main/java/GymNotebook/view/windows/ExerciseCreationWindow.java
+++ src/main/java/GymNotebook/view/windows/ExerciseCreationWindow.java
@@ -9,14 +9,133 @@ import java.util.List;
 public class ExerciseCreationWindow extends Window{
     private final ArrayList<String> exerciseTypes;
     private final Exercise exercise;
-    private State state;
+    private WindowState state;
 
-    enum State{
-        TitleEnter,
-        TypeSelection,
-        RepExercise,
-        TimeExercise
+    private class TitleInput implements WindowState{
+        public List<Command> HandleInput(String input) throws WindowException{
+            List<Command> commands = new ArrayList<>();
+
+            if(!input.isEmpty() ){
+                if(input.length()<20){
+                    exercise.setTitle(input);
+                    state = new TypeSelection();
+                }else{
+                    throw new WindowException("ERR: Title cannot be longer than 20 symbols");
+                }
+            }
+            else{
+                throw new WindowException("ERR: Title cannot be empty");
+            }
+
+            return commands;
+        }
+        public void Render(){
+            System.out.println("Enter Exercise Title");
+        }
     }
+
+    private class TypeSelection implements WindowState{
+        public List<Command> HandleInput(String input) throws WindowException{
+            List<Command> commands = new ArrayList<>();
+
+            try{
+                int selected = Integer.parseInt(input);
+                switch(selected){
+                    case 1:
+                        state = new RepExercise();
+                        break;
+                    case 2:
+                        state = new TimeExercise();
+                        break;
+                    default:
+                        throw new WindowException("ERR: Number out of range");
+                }
+            }catch (NumberFormatException e){
+                throw new WindowException("ERR: Invalid input");
+            }
+            return commands;
+        }
+        public void Render(){
+            for(int i = 0; i< exerciseTypes.size(); i++){
+                System.out.printf("%d. %s%n",i+1, exerciseTypes.get(i));
+            }
+            inputOptions.clear();
+            inputOptions.addFirst("[Number] - Choose option");
+        }
+    }
+
+    private class RepExercise implements WindowState{
+        public List<Command> HandleInput(String input) throws WindowException{
+            List<Command> commands = new ArrayList<>();
+
+            try{
+                int selected = Integer.parseInt(input);
+                switch(selected){
+                    case 1:
+                        SaveExercise saveExerciseCommand = new SaveExercise();
+                        saveExerciseCommand.SetExercise(exercise);
+                        commands.add(saveExerciseCommand);
+
+                        commands.add(new OpenNewRepSet());
+                        break;
+                    case 2:
+                        AddExerciseToCurrentWorkout AddExerciseCommand = new AddExerciseToCurrentWorkout();
+                        AddExerciseCommand.SetExercise(exercise);
+                        commands.add(AddExerciseCommand);
+
+                        commands.add(new GoBack());
+                        break;
+                    default:
+                        throw new WindowException("ERR: Number out of range");
+                }
+            }catch (NumberFormatException e){
+                throw new WindowException("ERR: Invalid input");
+            }
+
+            return commands;
+        }
+        public void Render(){
+            SendExerciseOverview();
+            SendOptions();
+        }
+    }
+
+    private class TimeExercise implements WindowState{
+        public List<Command> HandleInput(String input) throws WindowException {
+            List<Command> commands = new ArrayList<>();
+
+            try{
+                int selected = Integer.parseInt(input);
+                switch(selected){
+                    case 1:
+                        SaveExercise saveExerciseCommand = new SaveExercise();
+                        saveExerciseCommand.SetExercise(exercise);
+                        commands.add(saveExerciseCommand);
+
+                        commands.add(new OpenNewTimeSet());
+                        break;
+                    case 2:
+                        AddExerciseToCurrentWorkout AddExerciseCommand = new AddExerciseToCurrentWorkout();
+                        AddExerciseCommand.SetExercise(exercise);
+                        commands.add(AddExerciseCommand);
+
+                        commands.add(new GoBack());
+                        break;
+                    default:
+                        throw new WindowException("ERR: Number out of range.");
+                }
+            }catch (NumberFormatException e){
+                throw new WindowException("ERR: Invalid input");
+            }
+
+            return commands;
+        }
+        public void Render(){
+            SendExerciseOverview();
+            SendOptions();
+        }
+    }
+
 
     public ExerciseCreationWindow(Exercise exercise){
         this.exercise = exercise;
@@ -25,7 +144,7 @@ public class ExerciseCreationWindow extends Window{
 
         inputOptions.add("Enter exercise name");
 
-        state = State.TitleEnter;
+        state = new TitleInput();
 
         exerciseTypes = new ArrayList<>();
 
@@ -38,143 +157,21 @@ public class ExerciseCreationWindow extends Window{
 
     @Override
     public void SendBody(){
-        switch(state){
-            case TitleEnter:
-                SendTitleEnter();
-                break;
-            case TypeSelection:
-                SendTypeSelection();
-                break;
-            case RepExercise:
-            case TimeExercise:
-                SendExerciseOverview();
-                SendOptions();
-                break;
-
-        }
-    }
-
-    private void SendTitleEnter(){
-        System.out.println("Enter Exercise Title");
+        state.Render();
     }
 
     private void SendExerciseOverview(){
         System.out.println(exercise.toString());
     }
 
-    private void SendTypeSelection(){
-        for(int i = 0; i< exerciseTypes.size(); i++){
-            System.out.printf("%d. %s%n",i+1, exerciseTypes.get(i));
-        }
-        inputOptions.clear();
-        inputOptions.add("[Number] - Choose option");
-    }
-
     @Override
     public List<Command> HandleInput(String input){
         List<Command> commands = new ArrayList<>();
 
-        switch(state){
-            case TitleEnter:
-                HandleTitleEnter(input);
-                break;
-            case TypeSelection:
-                HandleTypeSelection(input);
-                break;
-            case RepExercise:
-                commands.addAll(HandleRepExercise(input));
-                break;
-            case TimeExercise:
-                commands.addAll(HandleTimeExercise(input));
-                break;
-
-        }
-
-        return commands;
-    }
-    private void HandleTitleEnter(String input){
-        if(!input.isEmpty()){
-            exercise.setTitle(input);
-            state = State.TypeSelection;
-        }
-        else{
-            info = "ERR: Title cannot be empty";
-        }
-    }
-
-    private void HandleTypeSelection(String input){
         try{
-            int selected = Integer.parseInt(input);
-            switch(selected){
-                case 1:
-                    state = State.RepExercise;
-                    break;
-                case 2:
-                    state = State.TimeExercise;
-                    break;
-                default:
-                    info = "ERR: Number out of range.";
-            }
-        }catch (NumberFormatException e){
-            info = "ERR: Invalid input.";
-        }
-    }
-
-    private List<Command> HandleRepExercise(String input){
-        List<Command> commands = new ArrayList<>();
-
-        try{
-            int selected = Integer.parseInt(input);
-            switch(selected){
-                case 1:
-                    SaveExercise saveExerciseCommand = new SaveExercise();
-                    saveExerciseCommand.SetExercise(exercise);
-                    commands.add(saveExerciseCommand);
-
-                    commands.add(new OpenNewRepSet());
-                    break;
-                case 2:
-                    AddExerciseToCurrentWorkout AddExerciseCommand = new AddExerciseToCurrentWorkout();
-                    AddExerciseCommand.SetExercise(exercise);
-                    commands.add(AddExerciseCommand);
-
-                    commands.add(new GoBack());
-                    break;
-                default:
-                    info = "ERR: Number out of range.";
-            }
-        }catch (NumberFormatException e){
-            info = "ERR: Invalid input.";
-        }
-
-        return commands;
-    }
-
-    private List<Command> HandleTimeExercise(String input){
-        List<Command> commands = new ArrayList<>();
-
-        try{
-            int selected = Integer.parseInt(input);
-            switch(selected){
-                case 1:
-                    SaveExercise saveExerciseCommand = new SaveExercise();
-                    saveExerciseCommand.SetExercise(exercise);
-                    commands.add(saveExerciseCommand);
-
-                    commands.add(new OpenNewTimeSet());
-                    break;
-                case 2:
-                    AddExerciseToCurrentWorkout AddExerciseCommand = new AddExerciseToCurrentWorkout();
-                    AddExerciseCommand.SetExercise(exercise);
-                    commands.add(AddExerciseCommand);
-
-                    commands.add(new GoBack());
-                    break;
-                default:
-                    info = "ERR: Number out of range.";
-            }
-        }catch (NumberFormatException e){
-            info = "ERR: Invalid input.";
+            commands.addAll(state.HandleInput(input));
+        }catch (WindowException e){
+            info = e.getMessage();
         }
 
         return commands;
